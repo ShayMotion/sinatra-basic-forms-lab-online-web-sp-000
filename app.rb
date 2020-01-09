235b213b19af9b005ae804888c5e17ecd4d20d74
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-01-09 16:35:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,12 @@ get '/' do
   erb: index
 end
 
-get ''
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
+end
